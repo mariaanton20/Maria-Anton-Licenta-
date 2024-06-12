48722333874092011ba39c81994825edf2704f1a
mariaanton20 <137863301+mariaanton20@users.noreply.github.com>
2024-06-12 12:21:29 +0300

    Add files via upload

---
+++ agentie/index.php
@@ -0,0 +1,33 @@
+<?php
+	session_set_cookie_params(0);
+	ob_start();
+	session_start();
+	$_SESSION['loggedIn'] = false;
+?>
+
+<!DOCTYPE html>
+<html lang="ro">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="img/logo.png" type="image/png">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="../agentieData/script.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+		<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+		
+		<title>Travel</title>
+	</head>
+	<body>
+
+	<header>
+
+	</header>
+	
+	<main>
+		
+	</main>
+
+	</body>
+</html>
